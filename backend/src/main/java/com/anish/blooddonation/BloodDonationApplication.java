58d817e99177909d8b4013fc69e1fbd7e76eb785
AnishPal006 <anishpaul865@gmail.com>
2026-09-20 00:08:54 +0530

    chore: Update backend configurations and dependencies

--- backend/src/main/java/com/anish/blooddonation/BloodDonationApplication.java
+++ backend/src/main/java/com/anish/blooddonation/BloodDonationApplication.java
@@ -2,11 +2,13 @@ package com.anish.blooddonation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BloodDonationApplication {
+
     public static void main(String[] args) {
-        // Launch the Spring Boot application.
         SpringApplication.run(BloodDonationApplication.class, args);
     }
 }
